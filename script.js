b024188cd057d80124b37c3da7a745bd9f99d646
asantagata <asantagata225@gmail.com>
2025-09-26 21:28:06 -0400

    fix to alignment bug

--- script.js
+++ script.js
@@ -1,61 +1,57 @@
 const set24HourTime = (bool) => {
     CONFIGS.timeIn24 = bool;
     renderYEAR();
+    selectCell(SELECTION);
 }
 
 const CELL_SIZE = 22;
 let YEAR = null;
+let SELECTION = 0;
 
 const COLORS = [
-    [
         'hsl(355, 74%, 57%)', 'hsl(359, 62%, 64%)', 'hsl(7, 76%, 45%)',
         'hsl(341, 69%, 60%)', 'hsl(6, 66%, 51%)', 'hsl(1, 79%, 72%)',
         'hsl(353, 64%, 37%)', 'hsl(0, 74%, 41%)', 'hsl(357, 76%, 62%)',
-    ], [
+    
         'hsl(13, 93%, 67%)', 'hsl(17, 81%, 74%)', 'hsl(25, 95%, 55%)',
         'hsl(359, 88%, 70%)', 'hsl(28, 85%, 61%)', 'hsl(19, 98%, 82%)',
         'hsl(11, 83%, 47%)', 'hsl(18, 93%, 51%)', 'hsl(15, 95%, 72%)',
-    ], [
+    
         'hsl(28, 26%, 60%)', 'hsl(32, 14%, 77%)', 'hsl(40, 28%, 48%)',
         'hsl(14, 21%, 63%)', 'hsl(48, 18%, 54%)', 'hsl(34, 31%, 75%)',
         'hsl(26, 16%, 40%)', 'hsl(33, 26%, 44%)', 'hsl(35, 28%, 66%)',
-    ], [
+    
         'hsl(41, 94%, 65%)', 'hsl(45, 82%, 72%)', 'hsl(55, 96%, 50%)',
         'hsl(29, 89%, 68%)', 'hsl(55, 76%, 59%)', 'hsl(47, 99%, 80%)',
         'hsl(39, 84%, 45%)', 'hsl(46, 94%, 49%)', 'hsl(43, 96%, 70%)',
-    ], [
+    
         'hsl(74, 44%, 59%)', 'hsl(78, 22%, 66%)', 'hsl(86, 46%, 47%)',
         'hsl(62, 39%, 62%)', 'hsl(91, 30%, 53%)', 'hsl(80, 49%, 74%)',
         'hsl(72, 34%, 39%)', 'hsl(79, 44%, 43%)', 'hsl(74, 42%, 64%)',
-    ], [
+    
         'hsl(124, 49%, 49%)', 'hsl(128, 37%, 56%)', 'hsl(136, 51%, 37%)',
         'hsl(110, 44%, 52%)', 'hsl(141, 41%, 43%)', 'hsl(137, 54%, 64%)',
         'hsl(122, 24%, 29%)', 'hsl(129, 49%, 33%)', 'hsl(126, 51%, 59%)',
-    ], [
+    
         'hsl(167, 60%, 52%)', 'hsl(171, 48%, 68%)', 'hsl(187, 62%, 35%)',
         'hsl(150, 55%, 58%)', 'hsl(184, 52%, 49%)', 'hsl(173, 75%, 70%)',
         'hsl(165, 45%, 27%)', 'hsl(169, 60%, 37%)', 'hsl(155, 62%, 60%)',
-    ], [
+    
         'hsl(199, 66%, 53%)', 'hsl(203, 54%, 75%)', 'hsl(211, 68%, 49%)',
         'hsl(188, 61%, 56%)', 'hsl(216, 58%, 47%)', 'hsl(205, 71%, 68%)',
         'hsl(197, 45%, 42%)', 'hsl(204, 76%, 30%)', 'hsl(195, 84%, 63%)',
-    ], [
+    
         'hsl(213, 27%, 71%)', 'hsl(217, 15%, 78%)', 'hsl(225, 36%, 59%)',
         'hsl(199, 22%, 74%)', 'hsl(230, 19%, 65%)', 'hsl(219, 32%, 89%)',
         'hsl(215, 17%, 38%)', 'hsl(222, 23%, 55%)', 'hsl(215, 29%, 76%)',
-    ], [
+    
         'hsl(249, 88%, 74%)', 'hsl(253, 76%, 81%)', 'hsl(261, 67%, 55%)',
         'hsl(235, 83%, 77%)', 'hsl(269, 80%, 68%)', 'hsl(255, 93%, 91%)',
         'hsl(247, 57%, 41%)', 'hsl(254, 88%, 58%)', 'hsl(251, 90%, 79%)',
-    ], [
+    
         'hsl(324, 85%, 71%)', 'hsl(331, 70%, 78%)', 'hsl(342, 87%, 59%)',
         'hsl(310, 80%, 74%)', 'hsl(344, 77%, 65%)', 'hsl(333, 90%, 86%)',
         'hsl(323, 75%, 51%)', 'hsl(332, 85%, 55%)', 'hsl(329, 87%, 76%)'
-    ]
-]
-
-const DEFAULT_KEYS = [
-    {}
 ]
 
 let CONFIGS = {
@@ -137,6 +133,7 @@ const renderYEAR = () => {
     const drawGridlines = () => {
 
         ctx.strokeStyle = "#13111d44";
+        ctx.beginPath();
 
         for (let i = 1; i < rows; i++) {
             ctx.moveTo(0.5, i * CELL_SIZE - 0.5);
@@ -181,7 +178,7 @@ const renderYEAR = () => {
                     button.style.background = 'var(--gentle)';
                     button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eraser-icon lucide-eraser"><path d="M21 21H8a2 2 0 0 1-1.42-.587l-3.994-3.999a2 2 0 0 1 0-2.828l10-10a2 2 0 0 1 2.829 0l5.999 6a2 2 0 0 1 0 2.828L12.834 21"/><path d="m5.082 11.09 8.828 8.828"/></svg>`;
                 } else {
-                    button.style.background = activity.isTop ? COLORS[sub.color][0] : COLORS[activity.color][sub.color];
+                    button.style.background = activity.isTop ? COLORS[sub.color * 9] : COLORS[activity.color * 9 + sub.color];
                     const text = document.createTextNode(sub.name);
                     button.appendChild(text);
                 }
@@ -195,7 +192,8 @@ const renderYEAR = () => {
                     });
                 } else {
                     button.addEventListener('click', () => {
-                        console.log('color', activity.color, sub.color);
+                        const colorId = sub.isErase ? 255 : activity.isTop ? (sub.color * 9) : (activity.color * 9 + sub.color);
+                        paintSELECTION(ctx, colorId)
                     });
                 }
                 return button;
@@ -207,20 +205,43 @@ const renderYEAR = () => {
     renderActivities();
 }
 
-const paintSelected = (index) => {
-
+const paintSELECTION = (ctx, colorId) => {
+    const row = Math.floor(SELECTION / 48) * CELL_SIZE;
+    const col = (SELECTION % 48) * CELL_SIZE;
+    if (colorId === 255) {
+        ctx.clearRect(col, row, CELL_SIZE, CELL_SIZE);
+    } else {
+        ctx.fillStyle = COLORS[colorId];
+        ctx.fillRect(col, row, CELL_SIZE, CELL_SIZE);
+    }
+    ctx.beginPath();
+    if (SELECTION % 48 !== 47) {
+        ctx.moveTo(col + CELL_SIZE - 0.5, row - 0.5);
+        ctx.lineTo(col + CELL_SIZE - 0.5, row + CELL_SIZE - 0.5);
+    }
+    if (Math.floor(SELECTION / 48) < daysInYear(YEAR.year) - 1) {
+        ctx.moveTo(col - 0.5, row + CELL_SIZE - 0.5);
+        ctx.lineTo(col + CELL_SIZE - 0.5, row + CELL_SIZE - 0.5);
+    }
+    ctx.stroke();
+    YEAR.cells[SELECTION] = colorId;
+    if (SELECTION < YEAR.cells.length - 1) {
+        selectCell(SELECTION + 1);
+    }
 }
 
 const selectCell = (index) => {
-    document.getElementById('selection').style.left = `${(index % 48) * CELL_SIZE}px`;
-    document.getElementById('selection').style.top = `${Math.floor(index / 48) * CELL_SIZE}px`;
+    const cornerRect = document.getElementById('corner').getBoundingClientRect();
+    document.getElementById('selection').style.left = `${cornerRect.width + (index % 48) * CELL_SIZE}px`;
+    document.getElementById('selection').style.top = `${cornerRect.height + Math.floor(index / 48) * CELL_SIZE}px`;
     window.requestAnimationFrame(() => window.requestAnimationFrame(() => {
         document.getElementById('selection').scrollIntoView({
             behavior: 'smooth',
             block: 'center',
             inline: 'center'
         });
-    }))
+    }));
+    SELECTION = index;
 }
 
 const handleCanvasClick = (event) => {
@@ -236,7 +257,7 @@ const handleCanvasClick = (event) => {
 
 const DEFAULT_YEAR = () => {
     return {
-        cells: new Uint8Array(48 * daysInYear((new Date()).getFullYear())),
+        cells: new Uint8Array(48 * daysInYear((new Date()).getFullYear())).fill(255),
         year: (new Date()).getFullYear(),
         activities: [
             {
@@ -280,7 +301,11 @@ const DEFAULT_YEAR = () => {
 const initialize = () => {
     YEAR = DEFAULT_YEAR()
     renderYEAR();
-    selectCell(0);
+    const current = new Date();
+    const jan1 = new Date(new Date().getFullYear(), 0, 1);
+    const diff = current - jan1;
+    const diffHalfHour = Math.floor(diff / (1000 * 60 * 30));
+    selectCell(diffHalfHour);
 }
 
 initialize();
